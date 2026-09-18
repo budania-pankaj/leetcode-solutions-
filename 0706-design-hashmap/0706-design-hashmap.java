56812f11e27cd50093a598caf97d715b8d6d7165
budania-pankaj <japabuva@gmail.com>
2026-09-18 22:39:58 +0530

    Time: 20 ms (66.45%), Space: 52.4 MB (67.24%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,29 +1,70 @@
-class MyHashMap {
-    private int[] arr = new int[1000001];
+import java.util.*;
 
-    public MyHashMap() {
-       for(int i = 0 ; i<arr.length; i++){
-        arr[i] = -1; 
-       }
-    }
+class Pair<U, V> {
+    U first;
+    V second;
 
-    public void put(int key, int value) {
-        arr[key] = value;
-    }
-
-    public int get(int key) {
-        return arr[key];
-    }
-
-    public void remove(int key) {
-        arr[key] = -1;
+    public Pair(U f, V s) {
+        first = f;
+        second = s;
     }
 }
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
+class Bucket {
+    private LinkedList<Pair<Integer, Integer>> list;
+
+    public Bucket() {
+        list = new LinkedList<>();
+    }
+
+    public int get(int key) {
+        for (Pair<Integer, Integer> p : list) {
+            if (p.first == key) {
+                return p.second;
+            }
+        }
+        return -1; // key not found
+    }
+
+    public void put(int key, int value) {
+        for (Pair<Integer, Integer> p : list) {
+            if (p.first == key) {
+                p.second = value;
+                return;
+            }
+        }
+        list.addFirst(new Pair<>(key, value));
+    }
+
+    public void remove(int key) {
+        list.removeIf(p -> p.first == key);
+    }
+}
+
+class MyHashMap {
+    private Bucket[] buckets;
+    private int keyRange = 769;
+
+    public MyHashMap() {
+        buckets = new Bucket[keyRange]; // allocate the array first
+        for (int i = 0; i < keyRange; i++) {
+            buckets[i] = new Bucket();
+        }
+    }
+
+    private int getBucketIndex(int key) {
+        return key % keyRange;
+    }
+
+    public void put(int key, int value) {
+        buckets[getBucketIndex(key)].put(key, value);
+    }
+
+    public int get(int key) {
+        return buckets[getBucketIndex(key)].get(key);
+    }
+
+    public void remove(int key) {
+        buckets[getBucketIndex(key)].remove(key);
+    }
+}
