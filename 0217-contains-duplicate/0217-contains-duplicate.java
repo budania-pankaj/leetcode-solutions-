c23569678b5203504d50c95aadbd0965e206a438
budania-pankaj <japabuva@gmail.com>
2026-09-19 13:44:03 +0530

    Time: 16 ms (89.32%), Space: 109 MB (15.26%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -13,11 +13,20 @@ class Solution {
     }
     return flag;
     */
+     /* <<<-----Approach-2 ----->>>>>
     Arrays.sort(nums);
     boolean flag = false;
     for(int i = 1; i< nums.length; i++){
         if(nums[i-1] == nums[i]) return (flag= true); 
     }
-     return flag;
+     return flag; */
+             Set<Integer> uniq = new HashSet<>();
+        for (int num : nums) {
+            if (!uniq.add(num)) {  // add() returns false if it's already present
+                return true;
+            }
+        }
+        return false;
+
     }
 }
