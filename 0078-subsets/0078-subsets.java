d9b848648b1376c2bfe1aea80e1ca9316e227f3e
budania-pankaj <japabuva@gmail.com>
2026-09-25 22:32:09 +0530

    Time: 1 ms (87.09%), Space: 44.2 MB (38.98%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<List<Integer>> ans= new ArrayList<>(); // global, like your 'arr'
+
+    public List<List<Integer>> subsets(int[] nums) {
+        printSubsets(0, nums, new ArrayList<>());
+        return ans;
+    }
+
+    private void printSubsets(int i, int[] nums, List<Integer> arr) {
+        if (i == nums.length) {
+            ans.add(arr);          
+            return;
+        }
+        printSubsets(i + 1, nums, arr);                  
+        List<Integer> take = new ArrayList<>(arr);       
+        take.add(nums[i]);
+        printSubsets(i + 1, nums, take);                   
+    }
+}
